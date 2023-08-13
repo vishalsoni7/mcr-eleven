64e533ba8e19b8fb956f7a38643787762b254696
vishal soni <vishsoni043@gmail.com>
2023-08-13 10:06:06 +0530

    initial-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-
 function App() {
   return (
     <div className="App">
       <h2>mcr-eleven</h2>
-      <FontAwesomeIcon icon={faHouse} />
     </div>
   );
 }
